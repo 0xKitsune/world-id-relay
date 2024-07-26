c8a7867dae9c50883c669a1d8104f7384eabec2c
0xKitsune <0xKitsune@protonmail.com>
2024-07-26 18:31:38 -0400

    update transaction relay trait

--- src/relay/mod.rs
+++ src/relay/mod.rs
@@ -1,1 +1,9 @@
-pub trait TxRelay {}
+pub mod tx_sitter;
+pub mod tx_sitter_aws;
+pub mod wallet;
+
+pub trait TransactionRelay {
+    //TODO: send transaction
+
+    //TODO: get receipt
+}
---
+++ src/relay/tx_sitter_aws.rs
@@ -0,0 +1,5 @@
+use tx_sitter_client::rpc::TxSitterRpcClient;
+
+use super::TxRelay;
+
+impl TxRelay for TxSitterRpcClient {}
